df77e75e57e770df8ba550c82eb6bec35cde33c9
tuanduog <tuanduog04@gmail.com>
2024-12-06 18:20:35 +0700

    update intent

---
+++ app/src/main/java/com/example/nhom14didong/Activity/Admin_Homepage.java
@@ -0,0 +1,28 @@
+package com.example.nhom14didong.Activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.ImageButton;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.nhom14didong.R;
+
+public class Admin_Homepage extends AppCompatActivity {
+private ImageButton btnQLTaiLieu;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.admin_homepage);
+        btnQLTaiLieu = findViewById(R.id.btnQLTaiLieu);
+        btnQLTaiLieu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Admin_Homepage.this, QuanLyTaiLieu.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
