90b084884bb767764b7e6601431dc0156096e7d0
tuanduog <tuanduog04@gmail.com>
2024-12-10 17:36:14 +0700

    update

--- app/src/main/java/com/example/nhom14didong/Activity/HomePage.java
+++ app/src/main/java/com/example/nhom14didong/Activity/HomePage.java
@@ -1,17 +1,31 @@
 package com.example.nhom14didong.Activity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.ImageButton;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import com.example.nhom14didong.Database.Database;
 import com.example.nhom14didong.R;
 
 public class HomePage extends AppCompatActivity {
+    ImageButton imgbtnDSTL;
+    FrameLayout flDSTaiLieu;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homepage);
+        imgbtnDSTL = findViewById(R.id.imgbtnDSTL);
+        flDSTaiLieu = findViewById(R.id.flDSTaiLieu);
+        imgbtnDSTL.setOnClickListener(v -> {
+            Intent intent = new Intent(HomePage.this, DanhSachTaiLieu.class);
+            startActivity(intent);
+        });
+        flDSTaiLieu.setOnClickListener(v -> {
+            Intent intent = new Intent(HomePage.this, DanhSachTaiLieu.class);
+            startActivity(intent);
+        });
     }
 }
